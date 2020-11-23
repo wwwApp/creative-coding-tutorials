da61574358350c6b4c5360d1de3884a9bad4fa3b
Woo Song <ws392@drexel.edu>
2020-11-23 14:00:07 -0500

    complete kinetic-typo-2

--- kinetic-typo-2/app.js
+++ kinetic-typo-2/app.js
@@ -1,30 +1,66 @@
+import { Visual } from './visual.js';
+
 class App {
 	constructor() {
-		this.canvas = document.createElement('canvas');
-		this.ctx = this.canvas.getContext('2d');
-		document.body.appendChild(this.canvas);
+		this.setWebgl();
 
-		this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+		// this.canvas = document.createElement('canvas');
+		// this.ctx = this.canvas.getContext('2d');
+		// document.body.appendChild(this.canvas);
 
-		window.addEventListener('resize', this.resize.bind(this), false);
-		this.resize();
+		// this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
-		requestAnimationFrame(this.animate.bind(this));
+		WebFont.load({
+			google: {
+				families: ['Hind:700'],
+			},
+			fontactive: () => {
+				this.visual = new Visual();
+
+				window.addEventListener(
+					'resize',
+					this.resize.bind(this),
+					false
+				);
+				this.resize();
+
+				requestAnimationFrame(this.animate.bind(this));
+			},
+		});
+	}
+
+	setWebgl() {
+		this.renderer = new PIXI.Renderer({
+			width: document.body.clientWidth,
+			height: document.body.clientHeight,
+			antialias: true,
+			transparent: false,
+			resolution: window.devicePixelRatio > 1 ? 2 : 1,
+			autoDensity: true,
+			powerPreference: 'high-performance',
+			backgroundColor: 0xffffff,
+		});
+
+		document.body.appendChild(this.renderer.view);
+
+		this.stage = new PIXI.Container();
 	}
 
 	resize() {
 		this.stageWidth = document.body.clientWidth;
 		this.stageHeight = document.body.clientHeight;
 
-		this.canvas.width = this.stageWidth * this.pixelRatio;
-		this.canvas.height = this.stageHeight * this.pixelRatio;
-		this.ctx.scale(this.pixelRatio, this.pixelRatio);
+		this.renderer.resize(this.stageWidth, this.stageHeight);
+
+		this.visual.show(this.stageWidth, this.stageHeight, this.stage);
 	}
 
 	animate(t) {
 		requestAnimationFrame(this.animate.bind(this));
 
-		this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+		this.visual.animate();
+
+		this.renderer.render(this.stage);
 	}
 }
 
